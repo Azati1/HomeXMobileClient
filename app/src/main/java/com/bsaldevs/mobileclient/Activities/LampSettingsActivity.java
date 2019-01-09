9a4f64d4da6cd1b4eca735af6275de7edc373eaa
snail02 <164074kk>
2019-01-09 22:40:31 +0500

    настройка лампочки

--- app/src/main/java/com/bsaldevs/mobileclient/Activities/LampSettingsActivity.java
+++ app/src/main/java/com/bsaldevs/mobileclient/Activities/LampSettingsActivity.java
@@ -3,16 +3,45 @@ package com.bsaldevs.mobileclient.Activities;
 import android.service.notification.StatusBarNotification;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import com.bsaldevs.mobileclient.R;
+import com.skydoves.colorpickerpreference.ColorEnvelope;
+import com.skydoves.colorpickerpreference.ColorListener;
+import com.skydoves.colorpickerpreference.ColorPickerView;
 
 public class LampSettingsActivity extends AppCompatActivity {
+
+    private ColorPickerView colorPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_light_settings);
         getSupportActionBar().hide();
+        colorPicker = findViewById(R.id.colorPickerView);
+        colorPicker.setPreferenceName("MyColorPickerView");
+
+
+   colorPicker.setColorListener(new ColorListener() {
+        @Override
+        public void onColorSelected(ColorEnvelope colorEnvelope) {
+            View curCol = findViewById(R.id.current_color_lamp);
+            curCol.setBackgroundColor(colorEnvelope.getColor());
+        }
+    });
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        colorPicker.saveData();
+    }
+
+
+
+
 
 }
